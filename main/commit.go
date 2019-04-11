c043c76d21c026b13122d63c20918e5ae93cc66b
Saulo Aflitos <sauloal@gmail.com>
2019-04-12 00:20:27 +0200

    commit

--- main/commit.go
+++ main/commit.go
@@ -1,11 +1,11 @@
 package main
 
-const IBROWSER_GIT_COMMIT_HASH     = "c85aba263e4a31da8fcf217398531d267e29e609)"
-const IBROWSER_GIT_COMMIT_AUTHOR   = "Saulo Aflitos (sauloal@gmail.com) 2019-04-11 19:50:48 +0200"
-const IBROWSER_GIT_COMMIT_COMMITER = "Saulo Aflitos (sauloal@gmail.com) 2019-04-11 19:50:48 +0200"
+const IBROWSER_GIT_COMMIT_HASH     = "ebbeebe4898955f41cbc867240664eb6cf4f5671)"
+const IBROWSER_GIT_COMMIT_AUTHOR   = "Saulo Aflitos (sauloal@gmail.com) 2019-04-12 00:19:03 +0200"
+const IBROWSER_GIT_COMMIT_COMMITER = "Saulo Aflitos (sauloal@gmail.com) 2019-04-12 00:19:03 +0200"
 const IBROWSER_GIT_COMMIT_NOTES    = ""
-const IBROWSER_GIT_COMMIT_TITLE    = "TODO: Double check distance matrix is working"
-const IBROWSER_GIT_STATUS          = "dirty"
-const IBROWSER_GIT_DIFF            = "44865d448a9b140a5db2902e7efbb2a1"
+const IBROWSER_GIT_COMMIT_TITLE    = "int32 matrix; no -race build parameter; new output name; check compression; check blocksize not too large; always stream when dump; onlyfirst debug parameter in vcf reader"
+const IBROWSER_GIT_STATUS          = "clean"
+const IBROWSER_GIT_DIFF            = "d41d8cd98f00b204e9800998ecf8427e"
 
 
